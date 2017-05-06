8e1f376f3b07700e863216d3692cb02c8a2a34a3
Luke Chatton <chattonluke@gmail.com>
2017-05-06 09:42:44 -0700

    Floor seconds

--- util/common.js
+++ util/common.js
@@ -92,7 +92,7 @@ exports.matchPlayerWithId = function(players, id, callback) {
 exports.toMMSS = function (sec_num) {
     var hours   = Math.floor(sec_num / 3600);
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+    var seconds = Math.floor(sec_num - (hours * 3600) - (minutes * 60));
 
     if (hours   < 10) {hours   = "0"+hours;}
     if (minutes < 10) {minutes = "0"+minutes;}
