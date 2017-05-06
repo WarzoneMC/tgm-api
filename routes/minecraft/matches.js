fc88eb101227a0cfabb333c7efb9c8bed4af2175
Luke Chatton <chattonluke@gmail.com>
2017-05-05 22:40:47 -0700

    Fix recent match response

--- routes/minecraft/matches.js
+++ routes/minecraft/matches.js
@@ -111,7 +111,7 @@ module.exports = function(app) {
                         })
                     }, function(err) {
                         res.json({
-                            match: match,
+                            matches: matches,
                             loadedMap: loadedMap
                         })
                     })
