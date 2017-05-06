64e69d8146a648c588aef7dc9ac1f5029944930b
Luke Chatton <chattonluke@gmail.com>
2017-05-05 17:31:17 -0700

    Return found deaths

--- routes/minecraft/deaths.js
+++ routes/minecraft/deaths.js
@@ -57,7 +57,7 @@ module.exports = function(app) {
                                 })
                             });
                         }, function(err) {
-                            res.json(deaths);
+                            res.json(foundDeaths);
                         })
                     })
                 })
