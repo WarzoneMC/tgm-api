bb16284553677141377cc281ebd36bec99491a1d
Lucas Cosolo <502badgamer@gmail.com>
2018-01-07 15:54:58 -0600

    Follow all redirects

--- routes/minecraft/servers.js
+++ routes/minecraft/servers.js
@@ -71,6 +71,7 @@ module.exports = function(app) {
         var options = {
             url: config.minehut.url + "/server/warzone/heartbeat",
             method: 'POST',
+            followAllRedirects: true,
             headers: {
                 'Content-Type': 'application/json',
                 'x-access-token': config.minehut.auth
