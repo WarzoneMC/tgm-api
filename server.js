bc0b1725baa69af0a17559a835f2516902f3369c
jorgeberrex <jorgeberrex@hotmail.com>
2018-02-17 19:03:32 -0500

    Add punishments route & latest punishments

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ require('./routes/minecraft/maps')(app);
 require('./routes/minecraft/leaderboards')(app);
 require('./routes/minecraft/matches')(app);
 require('./routes/minecraft/deaths')(app);
+require('./routes/minecraft/punishments')(app);
 
 const port = config.port;
 
