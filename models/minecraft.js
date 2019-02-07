b5d63c0593c50091eb6f7ed680fa43d646887bc3
AtditC <ardit@atdit.ch>
2019-02-07 21:35:29 +0100

    Wool Placement Tracking
    Tracks wool placements and gives 12 XP per wool placement.

--- models/minecraft.js
+++ models/minecraft.js
@@ -66,14 +66,15 @@ var MinecraftUser = new Schema({
     matches                 : [ObjectId],
     
     wool_destroys           : Number,
+    wool_placements         : Number,
     punishments             : [MinecraftPunishment]
 });
 function toJSON(obj, c) {
     if (c) obj = obj.toObject();
     delete obj.password;
     delete obj.ips;
-    obj.level = parseInt(0.6 * Math.sqrt(((obj.wins ? obj.wins : 0) * 10) + ((obj.losses ? obj.losses : 0) * 5) + ((obj.wool_destroys ? obj.wool_destroys : 0) * 3) + ((obj.kills ? obj.kills : 0) * 2)), 10) + 1
-    obj.levelRaw = 0.6 * Math.sqrt(((obj.wins ? obj.wins : 0) * 10) + ((obj.losses ? obj.losses : 0) * 5) + ((obj.wool_destroys ? obj.wool_destroys : 0) * 3) + ((obj.kills ? obj.kills : 0) * 2)) + 1
+    obj.level = parseInt(0.6 * Math.sqrt(((obj.wins ? obj.wins : 0) * 10) + ((obj.losses ? obj.losses : 0) * 5) + ((obj.wool_destroys ? obj.wool_destroys : 0) * 3) + ((obj.wool_placements ? obj.wool_placements : 0) * 12) + ((obj.kills ? obj.kills : 0) * 2)), 10) + 1
+    obj.levelRaw = 0.6 * Math.sqrt(((obj.wins ? obj.wins : 0) * 10) + ((obj.losses ? obj.losses : 0) * 5) + ((obj.wool_destroys ? obj.wool_destroys : 0) * 3) + ((obj.wool_placements ? obj.wool_placements : 0) * 12) + ((obj.kills ? obj.kills : 0) * 2)) + 1
     obj.xp = ((obj.wins ? obj.wins : 0) * 10) + ((obj.losses ? obj.losses : 0) * 5) + ((obj.wool_destroys ? obj.wool_destroys : 0) * 3) + ((obj.kills ? obj.kills : 0) * 2);
     return obj;
 }
--- routes/minecraft/matches.js
+++ routes/minecraft/matches.js
@@ -21,6 +21,17 @@ module.exports = function (app) {
     })
 
     /**
+     * Increments user's wool break count by 1.
+     *
+     * Called when user breaks a wool mid-match.
+     */
+    app.post('/mc/match/place_wool', verifyServer, (req, res) => {
+        MinecraftUser.update({ uuid: req.body.uuid }, { $inc: { wool_placements: 1 } }, (err) => {
+            res.json({});
+        })
+    })
+
+    /**
      * body: MatchLoadRequest.java
      */
     app.post('/mc/match/load', verifyServer, function(req, res) {
