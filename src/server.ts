4195d35fa8131109514ce5e64cd6716622fa90fc
Daniel Gulic <danielgulic@gmail.com>
2019-09-28 00:29:03 +1000

    init deaths route

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/mc/players', require('./routes/minecraft/players').default);
+app.use('/api/mc/deaths', require('./routes/minecraft/deaths').default);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Warzone API listening on port', port));
